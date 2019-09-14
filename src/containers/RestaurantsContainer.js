f6db2047146ac17422a609a1b0dd4f243aeacda2
Alison Golden <alisong779@yahoo.com>
2019-09-14 14:43:32 -0400

    Done.

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
+import { connect } from 'react-redux';
 
 class RestaurantsContainer extends Component {
 
+//passing addRestaurant to RestaurantInput props
+//passing restaurants and deleteRestaurant to Restaurants props
   render() {
     return (
       <div>
-        <RestaurantInput />
-        <Restaurants />
+        <RestaurantInput addRestaurant={this.props.addRestaurant} />
+        <Restaurants restaurants={this.props.restaurants} deleteRestaurant={this.props.deleteRestaurant}/>
       </div>
     )
   }
 }
 
-export default RestaurantsContainer
+const mapStateToProps = state => ({ restaurants: state.restaurants })
+
+const mapDispatchToProps = dispatch => ({
+  addRestaurant: text => dispatch({ type: "ADD_RESTAURANT", text }),
+  deleteRestaurant: id => dispatch({ type: "DELETE_RESTAURANT", id })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantsContainer)
+//exporting restaurants state to props
+//exporting dispatch actions addRestaurant and deleteRestaurant to props
